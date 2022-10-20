56ff01c463318605fbf3a34d469350df715d1da9
Imran Saleh <imran96saleh@gmail.com>
2022-10-20 13:31:28 +0500

    Adds some documentation to view function

--- src/main.rs
+++ src/main.rs
@@ -25,8 +25,11 @@ fn orbits(app: &App, _model: &Model, frame: Frame){
     // Get current time
     let tt = app.elapsed_frames() as f32;
     let tt2 = app.elapsed_frames() ;
+
     let dot_col = CORNFLOWERBLUE;
     let line_col = STEELBLUE;
+
+    // Size of dots
     let sz = 10.0;
      
     /* Each tuple consists of: 
@@ -35,7 +38,7 @@ fn orbits(app: &App, _model: &Model, frame: Frame){
     3) offset from origin (can be used for x or y direction) */
     let params = [(78.3, 250.0, 222.0), (1333.3, 555.0, 0.0)];
 
-    // plot dots and stores their positions to plot lines
+    // Draw dots and store their positions to draw lines
     let mut posns = Vec::new();
     for (vel, dist, offset) in params {
 	let xx = dist * ((tt/vel).sin())+offset;
@@ -47,22 +50,24 @@ fn orbits(app: &App, _model: &Model, frame: Frame){
 	}
     }
 
+    // Draw lines
     for ii in 1..posns.len() {
 	draw.line().start(posns[ii]).end(posns[ii-1])
 	    .weight(2.0).color(line_col);
     }
 
     // Clear the background to black
-    // if tt2 < 2{
     if tt2 < 2{
 	draw.background().color(BLACK);
     }
 
-    let mut path = String::from("/home/imran/Pictures/nannou/take1/");
-    let tt3 = format!("{:05}",tt2);
-    path = path + &tt3;
-    path.push_str(".png");
-    app.window(app.window_id()).unwrap().capture_frame(path);
+    // Functionality for outputting frames to png in order
+    // to make an mp4
+    // let mut path = String::from("/home/imran/Pictures/nannou/take1/");
+    // let tt3 = format!("{:05}",tt2);
+    // path = path + &tt3;
+    // path.push_str(".png");
+    // app.window(app.window_id()).unwrap().capture_frame(path);
 
     draw.to_frame(app, &frame).unwrap();
 }
